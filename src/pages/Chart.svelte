d062cdde4c868f303ad19823db395d55854820c5
Theo <theo.cousinet@epitech.eu>
2022-08-06 19:12:03 +0200

    UPDATE: graph size

--- src/pages/Chart.svelte
+++ src/pages/Chart.svelte
@@ -4,21 +4,24 @@
 
 <div in:fade={{ duration: 200 }} class="page">
   <div class="graph">
-    <span>
-      This infographic shows the importance of behaviors within your training path (AI for example).
-    </span>
-    <span>Don't forget to select this one when viewing.</span>
     <iframe
       scrolling="no"
       title="Interactive or visual content"
       src="flourish-chart.html"
       sandbox="allow-transparency allow-same-origin allow-forms allow-scripts allow-downloads allow-popups allow-popups-to-escape-sandbox allow-top-navigation-by-user-activation"
-      style="width: 100%; height: 500px;"
+      style="width: 100%; height: 80vh; min-height: 500px;"
       frameborder="0"
       allowtransparency
     />
-    <span>You can click on a skill to see the importance of the behaviours in it.</span>
-    <span>You can then click on a behavior to see in which modules it is available.</span>
+    <div class="text">
+      <span>
+        This infographic shows the importance of behaviors within your training path (AI for
+        example).
+      </span>
+      <span>Don't forget to select this one when viewing.</span>
+      <span>You can click on a skill to see the importance of the behaviours in it.</span>
+      <span>You can then click on a behavior to see in which modules it is available.</span>
+    </div>
   </div>
 </div>
 
@@ -37,6 +40,13 @@
 
     iframe {
       color-scheme: initial;
+      background-color: transparent;
+    }
+
+    .text {
+      display: flex;
+      flex-direction: column;
+      align-self: center;
     }
   }
 </style>
