0621065e359ae376634b7c596c1f65baabc2e9be
Theo <theo.cousinet@epitech.eu>
2023-02-17 00:09:20 +0100

    REFACTOR: store creation

--- src/store.ts
+++ src/store.ts
@@ -1,68 +1,41 @@
-import { type Updater, get, writable } from 'svelte/store'
+import { get, writable } from 'svelte/store'
 
-import type { MyBehavior, StudentInfo, TrainingPath } from './lib/competencies'
+import type {
+  // ImportInfo,
+  MyBehavior,
+  StudentInfo,
+  TrainingPath,
+} from './lib/competencies'
 import { loadBase64, removeLocalStorageItem, storeBase64 } from './lib/utils'
 
-type Preference = { speId?: TrainingPath; projectId?: string }
-function createPreference() {
-  const storeKey = 'preference'
-  const { subscribe, set, update } = writable<Preference | null>(null, (set) => {
+function persistentStoreFactory<T>(storeKey: string) {
+  const { subscribe, set, update } = writable<T | null>(null, (set) => {
     const data = loadBase64(storeKey)
     set(data)
   })
 
   return {
     subscribe,
-    set: (value: Preference) => {
+    set: (value: T) => {
       set(value)
       storeBase64(storeKey, value)
     },
-    update: (updater: Updater<Preference>) => {
-      update(updater as Updater<Preference | null>)
+    update: (updater: (value: T | null) => T) => {
+      update(updater)
       storeBase64(storeKey, get({ subscribe }))
     },
-  }
-}
-export const preferenceStore = createPreference()
-
-function createMyBehaviors() {
-  const storeKey = 'my_behaviors'
-  const { subscribe, set } = writable<MyBehavior[] | null>(null, (set) => {
-    const data = loadBase64(storeKey)
-    set(data)
-  })
-
-  return {
-    subscribe,
-    set: (value: MyBehavior[]) => {
-      set(value)
-      storeBase64(storeKey, value)
-    },
     reset: () => {
       set(null)
       removeLocalStorageItem(storeKey)
     },
   }
 }
-export const myBehaviorsStore = createMyBehaviors()
 
-function createStudentInfo() {
-  const storeKey = 'student'
-  const { subscribe, set } = writable<StudentInfo | null>(null, (set) => {
-    const data = loadBase64(storeKey)
-    set(data)
-  })
+type Preference = { speId?: TrainingPath; projectId?: string }
+export const preferenceStore = persistentStoreFactory<Preference>('preference')
 
-  return {
-    subscribe,
-    set: (value: StudentInfo) => {
-      set(value)
-      storeBase64(storeKey, value)
-    },
-    reset: () => {
-      set(null)
-      removeLocalStorageItem(storeKey)
-    },
-  }
-}
-export const studentInfoStore = createStudentInfo()
+export const myBehaviorsStore = persistentStoreFactory<MyBehavior[]>('my_behaviors')
+
+export const studentInfoStore = persistentStoreFactory<StudentInfo>('student')
+
+// export const lastImportInfoStore = persistentStoreFactory<ImportInfo>('import_info')
